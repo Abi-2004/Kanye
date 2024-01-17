23059806800cdd4c380682bc5325223763998f1d
abi <abiraldahal38@gmail.com>
2024-01-17 13:18:22 +0100

    hi

---
+++ menu.php
@@ -0,0 +1,32 @@
+<?php
+echo '<div class="barra">
+      <a href="index.php">Inicio</a>
+      <li class="dropdown-li">
+        <a href="gira.html">Gira</a>
+        <ul class="dropdown">
+          <a href="gira.html">Gira completa</a>
+          <a href="galeria.html">Galería</a>
+          <a href="entrada.html">Compra de entradas</a>
+        </ul>
+      </li>
+      <a href="bio.html">BioGrafia</a>
+      <a href="disco.html">Discografia</a>
+      <a href="fan.html">Fanclub</a>
+            <li class="dropdown-li">
+        <a href="#">Autor</a>
+        <ul class="dropdown">
+          <a href="#">Publicar</a>
+          <a href="#">Registrar</a>
+          <a href="#">Log in </a>
+        </ul>
+      </li>
+      
+      <!-- <li class="dropdown-li2">
+        <a href="merch.html">Tienda</a>
+        <ul class="dropdown2">
+          <a href="merch.html">Merch</a>
+          <a href="disks.html">Discos</a>
+        </ul>
+      </li>  -->
+    </div>';
+?>
