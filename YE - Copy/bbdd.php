3c3bba98dbfde7703a13ad307d6bbc363845df0d
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-01-22 09:19:24 +0100

    Update bbdd.php

--- YE - Copy/bbdd.php
+++ YE - Copy/bbdd.php
@@ -1,32 +1,48 @@
 <?php
-
-function connect() {
-    // Configuración de la conexión a la base de datos
-    $host = '44.195.114.107';
-    $username = 'abi';
-    $password = 'Dahal123';
-    $database = 'autor'; // Reemplaza con el nombre de tu base de datos
-
-    // Crear una conexión a la base de datos
-    $conn = new mysqli($host, $username, $password, $database);
-
-    // Verificar si hay errores en la conexión
-    if ($conn->connect_error) {
-        // Imprimir el mensaje de error si la conexión falló
-        die("Error de conexión a la base de datos: " . $conn->connect_error);
+    function connect_database()
+    {
+        $mysqli = new mysqli("44.195.114.107", "abi", "Dahal123", "kanye");
+        if($mysqli -> connect_errno)
+        {
+            echo "Fallo en la conexión: ".$mysqli->connect_errno;
+        }
+        return $mysqli;
     }
 
-    // Devolver la conexión para que pueda ser utilizada en otras partes del código
-    return $conn;
-}
+    function noticia()
+    {
+        $mysqli = connect_database();
+    
+        $sql = "SELECT id_noticia, nombre FROM noticias";
+    
+        $sentencia = $mysqli->prepare($sql);
+        if (!$sentencia) {
+            echo "Fallo en la preparación de la sentencia: " . $mysqli->errno;
+        }
+    
+        $ejecucion = $sentencia->execute();
+        if (!$ejecucion) {
+            echo "Fallo en la ejecucion: " . $mysqli->errno;
+        }
+    
+        $noticias = array(); 
+    
+        $id_noticia = -1; 
+        $nombre = "";
+    
+        $vincular = $sentencia->bind_result($id_noticia, $nombre);
+        if (!$vincular) {
+            echo "Fallo al vincular la sentencia: " . $mysqli->errno;
+        }
+    
+        while ($sentencia->fetch()) {
+            $noticia = array('idnoticia' => $id_noticia, 'nombre' => $nombre);
+            $noticias[] = $noticia; 
+        }
+    
+        $mysqli->close();
+        return $noticias;
+    }
+    
 
-// Llamar a la función connect y verificar el resultado
-$conn = connect();
-
-// Verificar si la conexión fue exitosa
-if ($conn) {
-    echo "Conexión exitosa a la base de datos.";
-} else {
-    echo "Error de conexión a la base de datos.";
-}
-?>
+    ?>
