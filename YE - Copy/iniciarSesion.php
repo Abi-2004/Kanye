18de4967b2471aaaeef3e5e242952f48dd90ce26
abi <abiraldahal38@gmail.com>
2024-01-23 12:43:35 +0100

    aaa

--- YE - Copy/iniciarSesion.php
+++ YE - Copy/iniciarSesion.php
@@ -4,7 +4,7 @@
     $sesionCorrecta = login($_POST["usuario"], $_POST["password"]);
     if($sesionCorrecta)
     {
-        $_SESSION["user"] = $_POST["user"];
+        $_SESSION["user"] = $_POST["usuario"];
         header("location: index.php");
     } else
     {
