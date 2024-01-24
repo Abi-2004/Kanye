88bb3d026df7dbe9d450a916e6378e012289463a
abi <abiraldahal38@gmail.com>
2024-01-24 14:45:54 +0100

    aa

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 
 
   
+    
 
 
 <?php include_once "menu.php"; ?>
---
+++ notc.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YEEZYS</title>
+    <link rel="stylesheet" href="css/comun.css">
+    <link rel="stylesheet" href="css/notc.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Noticia</h1>
+    </header>
+
+    <div class="barra">
+        <a href="index.html">Inicio</a>
+        <li class="dropdown-li">
+          <a href="gira.html">Gira</a>
+          <ul class="dropdown">
+            <a href="gira.html">Gira completa</a>
+            <a href="galeria.html">Galería</a>
+            <a href="entrada.html">Compra de entradas</a>
+          </ul>
+        </li>
+        <a href="bio.html">BioGrafia</a>
+        <a href="disco.html">Discografia</a>
+        <a href="fan.html">Fanclub</a>
+      </div>
+
+<div class="content">
+
+    <div class="notit" >
+        <H2>
+            Titulo de la noticia
+        </H2>
+    </div>
+
+<div class="notimg" > <img src="/img/noti1.webp" alt=""> </div>
+
+    <div class="notcont" >
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex possimus optio maiores iure magnam? Libero exercitationem quos eos consectetur recusandae numquam expl  Lorem ipsum dolor, sit amet consectetur adipisicing elit. In vel dolores impedit harum amet maiores voluptatum aliquam sint, inventore magni quod architecto illum quis facilis saepe temporibus suscipit, nulla sit. icabo sit cupiditate consequatur rerum, aperiam consequuntur adipisci distinctio. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime fuga quae voluptas alias pariatur mollitia in at ipsum nulla unde porro odit vero adipisci minima magni quo, aliquid facilis impedit!</p>
+    </div>
+
+    <div class="author-box">
+        <h4> 🔔 Publicado por Abiral</h4>
+    </div>
+
+</div>
+
+    <footer>
+       
+        <h4>COPYRIGHT © Abiral Dahal</h4>
+        <h5>2024</h5>
+      </footer>
+  
+  </body>
+  </html>
+  
