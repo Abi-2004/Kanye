1c9670e5b9ffd15edfb2bbaa2cc0e926fe2b8a71
Abi-2004 <abiraldahal38@gmail.com>
2024-01-27 23:00:18 +0100

    aa

--- reg.php
+++ reg.php
@@ -7,9 +7,7 @@
     <link rel="stylesheet" href="css/comun.css">
     <link rel="stylesheet" href="css/fan.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <!-- Include the reCAPTCHA Enterprise API script -->
-
-    <script src="https://www.google.com/recaptcha/enterprise.js?render=6Lf8X1wpAAAAAPP0WUdum8IHHQ_J2CoRhO2HDpLd"></script>
+   
     
   </head>
 <body>
@@ -44,9 +42,7 @@
                 <input type="email" id="email" name="email" required>
             </div>
 
-            <div class="g-recaptcha" data-sitekey="6Lf8X1wpAAAAAPP0WUdum8IHHQ_J2CoRhO2HDpLd"></div>
-
-            <button type="button" onclick="onClick(event)" class="btn-register">Registrar</button>
+            <button type="button"  class="btn-register">Registrar</button>
 
         </form>
     </div>
@@ -55,15 +51,7 @@
         <h4>COPYRIGHT © Abiral Dahal</h4>
         <h5>2024</h5>
     </footer>
-<script>
-  function onClick(e) {
-    e.preventDefault();
-    grecaptcha.enterprise.ready(async () => {
-      const token = await grecaptcha.enterprise.execute('6Lf8X1wpAAAAAPP0WUdum8IHHQ_J2CoRhO2HDpLd', {action: 'submit'});
-      document.getElementById("registration-form").submit();
-    });
-  }
-</script>
+
 
 </body>
 </html>
