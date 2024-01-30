820bbec5f1279a4331cd702facadde3cc6ee166e
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-01-30 11:46:21 +0000

    aa

--- notc.php
+++ notc.php
@@ -9,10 +9,7 @@
     <link rel="stylesheet" href="css/notc.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5960283468400996"
-     crossorigin="anonymous"></script>
-
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5960283468400996" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -21,47 +18,41 @@
         <h1>Noticia</h1>
     </header>
 
-<?php include_once 'menu.php'; ?>
+    <?php include_once 'menu.php'; ?>
 
-<div class="content">
+    <div class="content">
+        <?php
+        include_once 'bbdd.php';
 
+        if (isset($_GET['id_not'])) {
+            $noticiaId = $_GET['id_not'];
 
-<?php
-include_once 'bbdd.php';
+            $noticia = getNoticiaById($noticiaId);
 
-if (isset($_GET['id_not'])) {
-    $noticiaId = $_GET['id_not'];
+            // Display the title, image, and content
+            echo '<h2 class="notit">'.$noticia['titulo'].'</h2>';
+            echo '<img class="notimg" src="'.$noticia['img'].'" alt="imagen de noticia">';
+            echo '<p class="notcont">'.$noticia['content'].'</p>';
 
-    $noticia = getNoticiaById($noticiaId);
+            echo '<div class="author-box">';
+            $id = $noticia['id_aut'];
+            include_once 'bbdd.php';
+            $nombre = datosById($id);
 
-    // Display the title, image, and content
-    echo '<h2 class="notit" >'.$noticia['titulo'].'</h2>';
-    echo '<img class="notimg" src="'.$noticia['img'].'" alt="imagen de noticia">';
-    echo '<p class="notcont" >'.$noticia['content'].'</p>';
-
-    echo '<div class="author-box">';
-    $id = $noticia['id_aut'];
-    include_once 'bbdd.php';
-    $nombre = datosById($id);
-
-
-    echo '<h4> 🔔 Publicado por <b>'.$nombre.'</b></h4>';
-    echo ' <h4> Fecha: '.$noticia['fecha'].'</h4>';
-    echo '</div>';
-} else {
-
-    header("location: index.php");
-}
-
-?>
-
+            echo '<h4> 🔔 Publicado por <b>'.$nombre.'</b></h4>';
+            echo ' <h4> Fecha: '.$noticia['fecha'].'</h4>';
+            echo '</div>';
+        } else {
+            header("location: index.php");
+        }
+        ?>
+    </div>
 
     <footer>
-       
         <h4>COPYRIGHT © Abiral Dahal</h4>
         <h5>2024</h5>
-      </footer>
-  
-  </body>
-  </html>
-  
+    </footer>
+
+</body>
+
+</html>
