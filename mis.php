c69a15510e25273382cfe7ef5ba72784f2fe8684
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-01-31 09:19:34 +0000

    aaaa

--- mis.php
+++ mis.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YEEZYS</title>
     <link rel="stylesheet" href="css/comun.css">
-    <link rel="stylesheet" href="css/miss.css">
+    <link rel="stylesheet" href="css/mis.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
