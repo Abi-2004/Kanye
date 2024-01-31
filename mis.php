ef23412bcf97d15a8d2b66e1ae78ee062f41c2f2
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-01-31 09:12:36 +0000

    aa

---
+++ mis.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YEEZYS</title>
+    <link rel="stylesheet" href="css/comun.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body>
+    <header>
+        <h1>Kanye West</h1>
+    </header>
+    <?php include_once "menu.php"; ?>
+
+
+
+
+    <footer>
+        <h4>COPYRIGHT © Abiral Dahal</h4>
+        <h5>2024</h5>
+    </footer>
+</body>
+</html>
