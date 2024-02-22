9a8334a41fa768b83bb4cfe0d562df0b9833f836
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-02-22 11:48:32 +0000

    aaa

--- eliminar.php
+++ eliminar.php
@@ -1,22 +1,16 @@
 <?php
-    // Verificar si se recibió un ID válido
     if(isset($_GET['id_not']) && !empty($_GET['id_not'])) {
-        // Incluir el archivo con las funciones de base de datos
         include_once "bbdd.php";
 
-        // Obtener el ID de la noticia a eliminar desde la URL
         $idNoticia = $_GET['id_not'];
 
-        // Llamar a la función para eliminar la noticia
         if(eliminarNoticia($idNoticia)) {
             header("Location: mis.php");
             exit();
         } else {
-            // Si la eliminación falló, mostrar un mensaje de error
             echo "Error al eliminar la noticia.";
         }
     } else {
-        // Si no se recibió un ID válido, redirigir a la página de inicio o mostrar un mensaje de error
         header("Location: index.php");
         exit();
     }
